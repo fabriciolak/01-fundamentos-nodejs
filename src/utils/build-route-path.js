ef74bc79d986cd4ec98b1a7838af9aa6d916e7ca
Fabrício <fabriciolak12@gmail.com>
2023-03-07 11:05:16 -0300

    rotas com parâmetros regex

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,6 +1,9 @@
 // /users/:id
 export function buildRoutePath(path) {
   const routeParameterRegex = /:([a-zA-Z]+)/g
-  
+  const pathWithParams = path.replaceAll(routeParameterRegex, '(?<$1>[a-z0-9\-_]+)')
   console.log(Array.from(path.matchAll(routeParameterRegex)))
+
+  const pathRegex = new RegExp(`^${pathWithParams}`)
+  return pathRegex
 }
