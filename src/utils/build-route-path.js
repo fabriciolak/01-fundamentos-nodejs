78ff84d7c66d237766db35cf1ab32078852d174c
Fabrício <fabriciolak12@gmail.com>
2023-03-08 09:40:08 -0300

    regex para capturar query params

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -2,8 +2,8 @@
 export function buildRoutePath(path) {
   const routeParameterRegex = /:([a-zA-Z]+)/g
   const pathWithParams = path.replaceAll(routeParameterRegex, '(?<$1>[a-z0-9\-_]+)')
-  console.log(Array.from(path.matchAll(routeParameterRegex)))
+  // console.log(Array.from(path.matchAll(routeParameterRegex)))
 
-  const pathRegex = new RegExp(`^${pathWithParams}`)
+  const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`)
   return pathRegex
 }
